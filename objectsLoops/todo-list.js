5ced0cd6eb9708675d09e409b5a7e824a3b6ca8b
imane-daouah <imane.daouah@gmail.com>
2025-09-29 12:08:08 +0100

    commit

--- objectsLoops/todo-list.js
+++ objectsLoops/todo-list.js
@@ -1,45 +1,42 @@
-const todoList = [
-  { name: 'review course', dueDate: '2025-09-29' }
-];
+const todoList = [{
+  name: 'review course',
+  dueDate: '2025-09-29'
+}];
 
 renderTodoList();
-
 
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach((todoObject, index) => {
-    const { name, dueDate } = todoObject;
-
+  // Loop over every toDo object and append it to "todoListHTML"
+  // Show the objects inside the class "js-todo-list"
+  // Loop over evey delete button and add an eventListener that deletes the toDo and rerender the Tasks
+  todoList.forEach((todo, index) => {
+    const { name, dueDate } = todo;
     const html = `
-      <div class="todo-item">
-        <div>${name}</div>
-        <div>${dueDate}</div>
-        <button class="delete-todo-button js-delete-todo-button" data-index="${index}">
-          Delete
-        </button>
-      </div>
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="js-delete-button" data-index="${index}">Delete</button>
     `;
     todoListHTML += html;
   });
 
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
-  document.querySelectorAll('.js-delete-todo-button').forEach((button) => {
+  document.querySelectorAll('.js-delete-button').forEach(button => {
     button.addEventListener('click', (event) => {
-      const index = event.target.getAttribute('data-index'); 
-      todoList.splice(index, 1); 
-      renderTodoList(); 
+      const index = event.target.dataset.index;
+      todoList.splice(index, 1);
+      renderTodoList();
     });
   });
-}
 
+}
 
 document.querySelector('.js-add-todo-button')
   .addEventListener('click', () => {
     addTodo();
   });
-
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
@@ -48,7 +45,10 @@ function addTodo() {
   const dateInputElement = document.querySelector('.js-due-date-input');
   const dueDate = dateInputElement.value;
 
-  todoList.push({ name, dueDate });
+  // Add these values to the variable "todoList"
+if (name && dueDate) {
+    todoList.push({ name, dueDate });
+  }
 
   inputElement.value = '';
   dateInputElement.value = '';
